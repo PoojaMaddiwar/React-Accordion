40138e709fb38c0529ca80383a0ca8882d79a757
PoojaMaddiwar <poojamaddiwar23@gmail.com>
2024-04-03 00:42:48 +0530

    add dependencies and remove unneccessary code

---
+++ React-Accordion/src/components/data.js
@@ -0,0 +1,59 @@
+const data = [
+    {
+        id : "1",
+        question : "Whose project is this?",
+        answer : "This is Pooja's project."
+    },
+    {
+        id: "1",
+        question: "What is React?",
+        answer: "React is a JavaScript library for building user interfaces."
+    },
+    {
+        id: "2",
+        question: "What are the key features of React?",
+        answer: "Some key features of React include virtual DOM, component-based architecture, and one-way data flow."
+    },
+    {
+        id: "3",
+        question: "What is JSX?",
+        answer: "JSX is a syntax extension for JavaScript that allows you to write HTML-like code in React."
+    },
+    {
+        id: "4",
+        question: "What are React components?",
+        answer: "React components are reusable building blocks for UI elements in a React application."
+    },
+    {
+        id: "5",
+        question: "What is the purpose of state in React?",
+        answer: "State is used for storing and managing component-specific data in React."
+    },
+    {
+        id: "6",
+        question: "What is a functional component in React?",
+        answer: "A functional component in React is a JavaScript function that returns JSX to define a UI element."
+    },
+    {
+        id: "7",
+        question: "What is a class component in React?",
+        answer: "A class component in React is a JavaScript class that extends the React.Component class and defines a UI element."
+    },
+    {
+        id: "8",
+        question: "What are props in React?",
+        answer: "Props (short for properties) are used for passing data from parent to child components in React."
+    },
+    {
+        id: "9",
+        question: "What is the purpose of useEffect hook in React?",
+        answer: "The useEffect hook in React is used for performing side effects in function components, such as data fetching, subscriptions, or manually changing the DOM."
+    },
+    {
+        id: "10",
+        question: "What is the difference between state and props in React?",
+        answer: "State is managed within a component and can be changed over time, while props are passed from parent to child components and are immutable."
+    }
+];
+
+export default data;
---
+++ React-Accordion/src/components/index.jsx
@@ -0,0 +1,9 @@
+const Comp1 = ()=>{
+    return(
+        <>
+        <div>Accordion</div>
+        </>
+    )
+}
+
+export default Comp1;
